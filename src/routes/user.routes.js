a9bd55e49e619b4e8e4ba4a3ce841ac9e54a4c82
roni <codingloaded@gmail.com>
2025-03-27 09:41:31 +0530

    add user channel profile and watch history endpoints; improve error handlingcompleted pipelines in controller for channel profile and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   registerUser,
   reloadAccessToken,
+  updateAccountDetails,
+  updateAvatarImage,
+  updateCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwtToken } from "../middlewares/auth.middleware.js";
@@ -29,5 +36,17 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJwtToken, logoutUser);
 router.route("/reload-token").post(reloadAccessToken);
+router.route("/change-password").post(verifyJwtToken, changeCurrentPassword);
+router.route("/current-user").get(verifyJwtToken, getCurrentUser);
+router.route("/update-account").patch(verifyJwtToken, updateAccountDetails);
+router
+  .route("/upddate-avater")
+  .patch(verifyJwtToken, upload.single("avatar"), updateAvatarImage);
+router
+  .route("/upddate-cover")
+  .patch(verifyJwtToken, upload.single("coverImage"), updateCoverImage);
+
+router.route("/c/:username".get(verifyJwtToken, getUserChannelProfile));
+router.route("history").get(verifyJwtToken, getWatchHistory);
 
 export default router;
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,7 +2,7 @@ const asyncHandler = (fn) => async (req, res, next) => {
   try {
     return await fn(req, res, next);
   } catch (error) {
-    res.status(error.code || 500).json({
+    return res.status(error.code || 500).json({
       success: false,
       message: error.message,
     });
